726e4b4ad9c14412487d5c662c8b423b77ee2e96
전병길 <jeonbg@kakao.com>
2022-02-18 19:44:10 +0900

    Setup Repository

---
+++ styles/theme.ts
@@ -0,0 +1,20 @@
+import { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  color: {
+    black: {
+      base: '#333',
+      lighten1: '#727585',
+      lighten2: '#9a9ba7',
+    },
+    grey: {
+      base: '#c8cad2',
+      lighten1: '#f2f3f7',
+    },
+    white: '#fff',
+    orange: '#fb842d',
+    yellow: '#ffd400',
+  },
+};
+
+export default theme;
