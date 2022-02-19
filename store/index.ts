2932fd9c88d36b75bb469a4491e94211672e3586
전병길 <jeonbg@kakao.com>
2022-02-20 04:48:55 +0900

    feature : Setup Redux-saga

--- store/index.ts
+++ store/index.ts
@@ -1,15 +1,28 @@
-import { RootState } from './reducer/index';
 import { configureStore, Reducer } from '@reduxjs/toolkit';
+import { Store } from 'redux';
 import { createWrapper } from 'next-redux-wrapper';
+import createSagaMiddleware, { Task } from 'redux-saga';
 import rootReducer from './reducer';
+import { RootState } from './reducer/index';
+import rootSaga from './sagas';
 
-export const makeStore = () =>
-  configureStore({
+interface SagaStore extends Store {
+  sagaTask?: Task;
+}
+
+export const makeStore = () => {
+  const sagaMiddleware = createSagaMiddleware();
+  const store = configureStore({
     reducer: rootReducer as Reducer<RootState>,
+    middleware: [sagaMiddleware],
+    devTools: process.env.NODE_ENV === 'development',
   });
+  (store as SagaStore).sagaTask = sagaMiddleware.run(rootSaga);
+  return store;
+};
 
 const wrapper = createWrapper(makeStore, {
-  debug: process.env.NODE_ENV === 'development',
+  // debug: process.env.NODE_ENV === 'development',
 });
 
 export default wrapper;
